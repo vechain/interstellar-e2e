24100c348e2f7be412272730fce8184bbe65cf2c
otherview <otherview@gmail.com>
2026-04-10 16:20:23 +0100

    use compiled contracts

--- network/setup/network.go
+++ network/setup/network.go
@@ -10,10 +10,8 @@ import (
 )
 
 const (
-	defaultThorRepo = "https://github.com/vechain/thor"
-	// defaultThorBranch points to the branch containing EIP-6780 (opSuicide6780).
-	// Update to "evm-upgrades" once vechain/thor#1590 is merged.
-	defaultThorBranch = "moglu/eip6780"
+	defaultThorRepo   = "https://github.com/vechain/thor"
+	defaultThorBranch = "evm-upgrades"
 )
 
 // BuildNetwork constructs the 3-node network configuration for interstellar testing.
