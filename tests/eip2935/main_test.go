b79e72b5a9b533b8019faec0d89eb0983147402e
moglu2017 <mog.lu2017@outlook.com>
2026-05-19 16:03:39 +0800

    Remove branch target

--- tests/eip2935/main_test.go
+++ tests/eip2935/main_test.go
@@ -10,6 +10,5 @@ import (
 var nodeURL string
 
 func TestMain(m *testing.M) {
-	os.Setenv("THOR_BRANCH", "718bd161a70ba3ab7deadff6bce639ea95e2b546")
 	os.Exit(helper.RunTestMain(m, &nodeURL, nil))
 }
